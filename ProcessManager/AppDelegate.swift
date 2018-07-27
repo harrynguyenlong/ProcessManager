2d8a35cd35c0910437947a9a8ccd6b51d5f18502
NGUYEN BA LONG <Long.Nguyen@metropolia.fi>
2018-07-27 12:05:27 +0300

    Initial Commit

---
+++ ProcessManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProcessManager
+//
+//  Created by Nguyen Ba Long on 27/07/2018.
+//  Copyright © 2018 Nguyen Ba Long. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ProcessManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProcessManager
+//
+//  Created by Nguyen Ba Long on 27/07/2018.
+//  Copyright © 2018 Nguyen Ba Long. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
